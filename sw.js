af39850bc29448fe016f40c3865a4a7fc0e11f2f
TDJ-Hypno <hypno@poczta.onet.pl>
2025-07-10 08:07:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('install', e =>
   e.waitUntil(
-    caches.open('v1').then(c=>c.addAll(['/', 'index.html', 'manifest.json', 'ikona_48.png']))
+    caches.open('v1').then(cache =>
+      cache.addAll(['/', 'index.html', 'manifest.json', 'ikona_48.png'])
+    )
   )
 );
 self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
 );
